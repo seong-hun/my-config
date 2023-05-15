96ea31f33307b10d4c50a8f1f2e2b22f45edc79d
Seong-hun Kim <kshoon92@gmail.com>
2023-05-16 08:07:49 +0900

    Disable vimtex indent amphasand option

--- nvim/after/plugin/vimtex.rc.vim
+++ nvim/after/plugin/vimtex.rc.vim
@@ -1,8 +1,5 @@
 syntax enable
 
-" let g:vimtex_view_method = 'sioyek'
-" let g:vimtex_view_sioyek_exe = '/Applications/sioyek.app/Contents/MacOS/sioyek'
-" let g:vimtex_callback_progpath ='/usr/local/bin/nvim'
 let g:tex_flavor='latex'
 let g:vimtex_view_method = 'skim'
 let g:vimtex_view_skim_sync = 0
@@ -11,6 +8,7 @@ let g:vimtex_quickfix_open_on_warning = 0
 let g:vimtex_complete_img_use_tail = 1
 let g:matchup_override_vimtex = 1
 let g:matchup_matchparen_deferred = 1  " for performance
+let g:vimtex_indent_on_ampersands = 0
 
 
 augroup latexSurround
--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,101 +1,101 @@
 local M = {}
 
 function M.setup()
-	local telescope = require('telescope')
-	local actions = require('telescope.actions')
+  local telescope = require('telescope')
+  local actions = require('telescope.actions')
 
-	local config = {
-		defaults = {
-			mappings = {
-				n = {
-					["q"] = actions.close,
-					["<c-d>"] = require('telescope.actions').delete_buffer,
-				},
-				i = {
-					["<C-h>"] = "which_key",
-					["<c-d>"] = require('telescope.actions').delete_buffer,
-				}
-			},
-			file_ignore_patterns = {
-				"%.pdf",
-			},
-		},
-		pickers = {
-			buffers = {
-				-- ignore_current_buffer = true,
-				sort_mru = true,
-			},
-			find_files = {
-				mappings = {
-					n = {
-						["cd"] = function(prompt_bufnr)
-							local selection = require("telescope.actions.state").get_selected_entry()
-							local dir = vim.fn.fnamemodify(selection.path, ":p:h")
-							require("telescope.actions").close(prompt_bufnr)
-							-- Depending on what you want put `cd`, `lcd`, `tcd`
-							vim.cmd(string.format("silent lcd %s", dir))
-						end
-					}
-				}
-			},
-		},
-		extensions = {
-			fzf = {
-				fuzzy = true,                    -- false will only do exact matching
-				override_generic_sorter = true,  -- override the generic sorter
-				override_file_sorter = true,     -- override the file sorter
-				case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-																				 -- the default case_mode is "smart_case"
-			}
-		},
-	}
+  local config = {
+    defaults = {
+      mappings = {
+        n = {
+          ["q"] = actions.close,
+          ["<c-d>"] = require('telescope.actions').delete_buffer
+        },
+        i = {
+          ["<C-h>"] = "which_key",
+          ["<c-d>"] = require('telescope.actions').delete_buffer
+        }
+      },
+      file_ignore_patterns = {"%.pdf"}
+    },
+    pickers = {
+      buffers = {
+        -- ignore_current_buffer = true,
+        sort_mru = true
+      },
+      find_files = {
+        mappings = {
+          n = {
+            ["cd"] = function(prompt_bufnr)
+              local selection =
+                  require("telescope.actions.state").get_selected_entry()
+              local dir = vim.fn.fnamemodify(selection.path, ":p:h")
+              require("telescope.actions").close(prompt_bufnr)
+              -- Depending on what you want put `cd`, `lcd`, `tcd`
+              vim.cmd(string.format("silent lcd %s", dir))
+            end
+          }
+        }
+      }
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
+      }
+    }
+  }
 
-	local default_opts = { noremap = true, silent = true }
-	local function nmap(key, map) vim.api.nvim_set_keymap("n", key, map, default_opts) end
+  local default_opts = {noremap = true, silent = true}
+  local function nmap(key, map)
+    vim.api.nvim_set_keymap("n", key, map, default_opts)
+  end
 
-	nmap("<Leader>ff", "<CMD>lua require('telescope.builtin').find_files()<CR>")
-	nmap("<Leader>fg", "<CMD>lua require('telescope.builtin').live_grep()<CR>")
-	nmap("<Leader>fb", "<CMD>lua require('telescope.builtin').buffers()<CR>")
-	-- nmap("<Leader>fb", "<CMD>lua require('config.telescope').my_buffers()<CR>")
-	nmap("<Leader>fh", "<CMD>lua require('telescope.builtin').help_tags()<CR>")
-	nmap("<Leader>gc", "<CMD>lua require('telescope.builtin').git_commits()<CR>")
-	nmap("<Leader>gb", "<CMD>lua require('telescope.builtin').git_branches()<CR>")
+  nmap("<Leader>ff", "<CMD>lua require('telescope.builtin').find_files()<CR>")
+  nmap("<Leader>fg", "<CMD>lua require('telescope.builtin').live_grep()<CR>")
+  nmap("<Leader>fb", "<CMD>lua require('telescope.builtin').buffers()<CR>")
+  -- nmap("<Leader>fb", "<CMD>lua require('config.telescope').my_buffers()<CR>")
+  nmap("<Leader>fh", "<CMD>lua require('telescope.builtin').help_tags()<CR>")
+  nmap("<Leader>gc", "<CMD>lua require('telescope.builtin').git_commits()<CR>")
+  nmap("<Leader>gb", "<CMD>lua require('telescope.builtin').git_branches()<CR>")
 
-	telescope.load_extension('fzf')
-	telescope.setup(config)
+  telescope.load_extension('fzf')
+  telescope.setup(config)
 end
 
-
 function M.my_buffers(opts)
-	local action_state = require('telescope.actions.state')
-	local actions = require('telescope.actions')
+  local action_state = require('telescope.actions.state')
+  local actions = require('telescope.actions')
 
-	opts = opts or {}
-	-- opts.previewer = false
-	-- opts.sort_lastused = true
-	-- opts.show_all_buffers = true
-	-- opts.shorten_path = false
-	opts.attach_mappings = function(prompt_bufnr, map)
-		local delete_buf = function()
-			local current_picker = action_state.get_current_picker(prompt_bufnr)
-			local multi_selections = current_picker:get_multi_selection()
+  opts = opts or {}
+  -- opts.previewer = false
+  -- opts.sort_lastused = true
+  -- opts.show_all_buffers = true
+  -- opts.shorten_path = false
+  opts.attach_mappings = function(prompt_bufnr, map)
+    local delete_buf = function()
+      local current_picker = action_state.get_current_picker(prompt_bufnr)
+      local multi_selections = current_picker:get_multi_selection()
 
-			if next(multi_selections) == nil then
-				local selection = action_state.get_selected_entry()
-				-- actions.close(prompt_bufnr)
-				vim.api.nvim_buf_delete(selection.bufnr, {force = true})
-			else
-				-- actions.close(prompt_bufnr)
-				for _, selection in ipairs(multi_selections) do
-					vim.api.nvim_buf_delete(selection.bufnr, {force = true})
-				end
-			end
-		end
-		map('i', '<C-X>', delete_buf)
-		return true
-	end
-	require('telescope.builtin').buffers(opts)
-	-- require('telescope.builtin').buffers(require('telescope.themes').get_dropdown(opts))
+      if next(multi_selections) == nil then
+        local selection = action_state.get_selected_entry()
+        -- actions.close(prompt_bufnr)
+        vim.api.nvim_buf_delete(selection.bufnr, {force = true})
+      else
+        -- actions.close(prompt_bufnr)
+        for _, selection in ipairs(multi_selections) do
+          vim.api.nvim_buf_delete(selection.bufnr, {force = true})
+        end
+      end
+    end
+    map('i', '<C-X>', delete_buf)
+    return true
+  end
+  require('telescope.builtin').buffers(opts)
+  -- require('telescope.builtin').buffers(require('telescope.themes').get_dropdown(opts))
 end
 
 return M
